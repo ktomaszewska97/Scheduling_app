a05ed1726c004cb424620f4041c6c5d90424cd23
Karolina Tomaszewska <k.tomaszewska.97@gmail.com>
2021-06-13 14:38:35 -0400

    Added profile mage

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, profile_view
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^profile/$', profile_view),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.models import User
+from events.models import Event
 
 
 class LoginView(DjangoLoginView):
@@ -12,3 +15,14 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "accounts/register.html"
     success_url = reverse_lazy("accounts:login")
+
+
+def profile_view(request):
+    user = request.user
+    event = Event.objects.all().filter(owner_id=user.id)
+    context = {
+        'user': user,
+        'events': event,
+    }
+    template = 'accounts/profile.html'
+    return render(request, template, context)
